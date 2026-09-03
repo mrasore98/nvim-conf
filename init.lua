7345dd3198cd4e5f71f11d3a12318e358bd71553
Mathias Alexander <mathias.alexander@qtimaging.com>
2026-09-02 20:44:53 -0700

    Add floating terminal

--- init.lua
+++ init.lua
@@ -50,10 +50,10 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>qq', ':quit<CR>', {desc = "Quit"})
 vim.keymap.set('n', '<leader>rr', ':restart<CR>', {desc = "Restart"})
 -- window navigation
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
 --buffer navigation
 vim.keymap.set('n', '<S-h>', ':bp<CR>', {desc = "Next buffer"})
 vim.keymap.set('n', '<S-l>', ':bn<CR>', {desc = "Previous buffer"})
@@ -126,3 +126,38 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+
+-- TERMINAL
+local terminal_buf
+
+local function toggle_terminal()
+    if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
+        for _, win in ipairs(vim.fn.win_findbuf(terminal_buf)) do
+            vim.api.nvim_win_close(win, true)
+            return
+        end
+    end
+
+    local new_terminal = not terminal_buf or not vim.api.nvim_buf_is_valid(terminal_buf)
+    if new_terminal then terminal_buf = vim.api.nvim_create_buf(false, true) end
+
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+    vim.api.nvim_open_win(terminal_buf, true, {
+        relative = 'editor',
+        width = width,
+        height = height,
+        row = math.floor((vim.o.lines - height) / 2),
+        col = math.floor((vim.o.columns - width) / 2),
+        style = 'minimal',
+        border = 'rounded',
+    })
+    if new_terminal then
+        local shell = vim.o.shell:gsub('^"(.*)"$', '%1')
+        vim.fn.termopen({ shell })
+    end
+    vim.cmd.startinsert()
+end
+
+vim.keymap.set({ 'n', 't' }, '<C-_>', toggle_terminal, { desc = 'Toggle terminal' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
